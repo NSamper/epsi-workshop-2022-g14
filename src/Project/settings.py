dd34c126a2c6bfe18db7bc766a1666af3bf295b5
Nacedo Nocta <nsamper@peoplespheres.com>
2022-10-10 20:05:49 +0200

    fixed settings typo

--- src/Project/settings.py
+++ src/Project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'WebApp.apps.WebappConfig',
+    'MainWebApp.apps.MainwebappConfig'
 ]
 
 MIDDLEWARE = [
